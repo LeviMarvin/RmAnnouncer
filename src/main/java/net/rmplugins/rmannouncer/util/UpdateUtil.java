3f8555b17e564bf170bfef94bed615d38df636c6
Levi Marvin <levimarvin2020@outlook.com>
2021-05-26 15:24:07 +0800

    20210526
    152400

--- src/main/java/net/rmplugins/rmannouncer/util/UpdateUtil.java
+++ src/main/java/net/rmplugins/rmannouncer/util/UpdateUtil.java
@@ -11,6 +11,27 @@ public class UpdateUtil {
     }
 
     public static String getMainVersion() {
+        URL url = null;
+        HttpsURLConnection connection = null;
+        String version;
+        try {
+            Properties props=System.getProperties();
+            String osName = props.getProperty("os.name");
+            String osArch = props.getProperty("os.arch");
+            String osVersion = props.getProperty("os.version");
+            url = new URL("https://api.rmplugins.net/v1/static/check");
+            connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Content-type", "application/json");
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent",
+                    "RmAnnouncer/1.0 (release; " + osName + " " + osArch + " " + osVersion + ") RmPlugins-Agent/1.0 (static; SSL; Java 11)");
+            connection.setInstanceFollowRedirects(false);
+            connection.connect();
+        }catch (Exception e) {
+            sendError(e);
+        }
         return "";
     }
 
